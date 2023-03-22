970cfda66920e056d5339a0959c9a154e60351fb
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-03-22 17:17:50 -0300

    Update index.js

--- aula16-exercicio/index.js
+++ aula16-exercicio/index.js
@@ -1,6 +1,7 @@
 function calcular(){
     let primeiro = Number(document.getElementById('primeiro_numero').value)
     let segundo = Number(document.getElementById('segundo_numero').value)
+    document.getElementById('res').innerHTML = ''
 
     for (let i=1; i<=segundo; i++){
         document.getElementById('res').innerHTML += `<p>${primeiro} x ${i} = ${primeiro*i}</p>`
